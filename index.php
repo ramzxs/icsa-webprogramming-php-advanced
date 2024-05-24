11bf9247424dcc92a14a0040b62359831dd05c25
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-05-24 11:08:26 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us - SiteName</title>
+</head>
+<body>
+    <!-- header></header -->
+
+    <h1>CONTACT US</h1>
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <td>Name:</td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>Phone:</td>
+            <td>
+                <input type="tel">
+            </td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td>
+                <input type="email">
+            </td>
+        </tr>
+        <tr>
+            <td>Message:</td>
+            <td>
+                <textarea rows="10" cols="80"></textarea>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" value="Send">
+            </td>
+        </tr>
+    </table>
+    
+    <!-- <footer></footer> -->
+</body>
+</html>
